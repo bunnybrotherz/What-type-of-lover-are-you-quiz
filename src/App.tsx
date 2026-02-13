cdadafb423638dce5b4d118d2484152d9649b103
Jeslyn Wang <jeslynwjx@gmail.com>
2026-02-13 21:55:17 +0800

    try

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/What-type-of-lover-are-you-quiz">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
